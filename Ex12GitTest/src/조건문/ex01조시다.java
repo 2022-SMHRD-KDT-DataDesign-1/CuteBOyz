d2b24846d774254fcd8196ee87473ae53bf1d021
smhrd <smhrd@59.0.234.70>
2023-01-20 09:50:03 +0900

    2032-01-20/조시다/조건문파일

---
+++ Ex12GitTest/src/조건문/ex01조시다.java
@@ -0,0 +1,15 @@
+package 조건문;
+
+public class ex01조시다 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(true) {
+			System.out.println("끝내줘용");
+		}
+		
+		
+
+	}
+
+}
