035d31a49e1ccaa5cab719167d898390853dccb2
smhrd <smhrd@59.0.234.70>
2023-01-20 10:07:01 +0900

    2023-01-16-조시다마야-반복무노ㅘㅇ셩

---
+++ Ex12GitTest/src/반복문/ex01반복문.java
@@ -0,0 +1,14 @@
+package 반복문;
+
+public class ex01반복문 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 0 ; i<5; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
